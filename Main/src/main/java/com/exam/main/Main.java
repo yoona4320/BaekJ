3fd90aa3459c973a0d6cd57afb2d8543a9964c82
yoona <yoona4320@naver.com>
2022-11-24 10:04:15 +0900

    10430번

---
+++ Main/src/main/java/com/exam/main/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();		
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		int d = (a+b)%c;
+		int e = ((a%c)+(b%c))%c;
+		int f = (a*b)%c;
+		int g = ((a%c)*(b%c))%c;
+		
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		
+	}
+
+}
